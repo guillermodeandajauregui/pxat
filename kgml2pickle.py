094d1ff4761497f01d3749c408a16f58f5a8a1de
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-07-02 16:41:24 -0500

    use BioPython, keep all kgml elements in nx graph

--- kgml2pickle.py
+++ kgml2pickle.py
@@ -1,7 +1,9 @@
 import argparse
 import networkx as nx
-from keggparser import parse_KGML
+
+from Bio.KEGG.KGML import KGML_parser
 import pickle
+
 
 
 parser = argparse.ArgumentParser(description='Create a DiGraph from KGML file.')
@@ -10,12 +12,23 @@ parser.add_argument('--pickle', type=argparse.FileType('w'), required=True)
 
 args = parser.parse_args()
 
-g = parse_KGML.KGML2Graph(args.kgml)[1]
 
-print g.nodes()
+#then parse KGML file and return pathway object
+pathway = KGML_parser.parse(args.kgml).next()
 
-nx.write_gpickle(g,
-                 "/tmp/hola.p")
 
-#pickle.dump(g, args.pickle)
+# make genes unique
+uentries = {}
+for e in pathway.entries:
+    k = pathway.entries[e].graphics[0].name
+    uentries[k] = e
 
+
+g = nx.DiGraph()
+# copy relations to edges
+for relation in pathway.relations:
+    g.add_edge( uentries[relation.entry1.graphics[0].name],
+                uentries[relation.entry2.graphics[0].name], relation=relation)
+
+# write to pickle
+nx.gpickle.write_gpickle(g, args.pickle)
