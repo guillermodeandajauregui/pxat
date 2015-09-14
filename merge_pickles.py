ba489d10b5f6050c468e22eb19f426d677ae5512
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-09-14 12:39:24 -0500

    rewrite using lib

--- merge_pickles.py
+++ merge_pickles.py
@@ -1,35 +1,14 @@
 import argparse
 import networkx as nx
-from Bio.KEGG.KGML import KGML_parser
+from pxat import merge_pathway_subgraphs
 
 
-parser = argparse.ArgumentParser(description='Create a DiGraph from KGML file.')
+parser = argparse.ArgumentParser(description='Merge pickles.')
 parser.add_argument('--keggpickles', nargs='+', type=argparse.FileType('r'), required=True)
 parser.add_argument('--outpickle', type=argparse.FileType('w'), required=True)
 
 args = parser.parse_args()
 
+h = merge_pathway_subgraphs(*[nx.gpickle.read_gpickle(p) for p in args.keggpickles])
 
-# new graph
-h = nx.DiGraph()
-
-for p in args.keggpickles:
-    g = nx.gpickle.read_gpickle(p)
-
-    for n in g.nodes():
-
-        if n in h.node:
-            h.node[n]['pathways'].update(g.node[n]['pathways'])
-        else:
-            h.add_node(n, pathways=g.node[n]['pathways'])
-    
-    for e in g.edges():
-        edge_data = h.get_edge_data(*e)
-        if edge_data:
-            edge_data['pathways'].update(g.get_edge_data(*e)['pathways'])
-            h.add_edge( *e, attr_dict=edge_data)
-        else:
-            h.add_edge( *e, attr_dict=g.get_edge_data(*e))
-            
-# write to pickle
 nx.gpickle.write_gpickle(h, args.outpickle)
