cd0f8c881884d792b1958c545aa0bee8638cd955
X <x@X-SHATTERDOME.dedo>
2015-09-24 21:10:06 -0500

    Added various Jaccard index functions

--- pxat/__init__.py
+++ pxat/__init__.py
@@ -5,7 +5,9 @@ from pprint import pprint
 
 
 def pathway_jin( p1, p2, g ):
-
+    """
+    returns Jaccard index between two canonical pathways in the network already defined in the newtork
+    """
     pw1  = []
     pw2  = []
 
@@ -17,7 +19,29 @@ def pathway_jin( p1, p2, g ):
 
     return float(len(set(pw1).intersection(set(pw2)))) / float(len(set(pw1).union(set(pw2))))
 
+def geneset_jin(pw1, pw2):
+    """
+    returns Jaccard index between two genesets
+    
+    """
+    return float(len(set(pw1).intersection(set(pw2)))) / float(len(set(pw1).union(set(pw2))))
 
+def graph_jin(g1, g2):
+    """
+    returns Jaccard index between two genesets
+    
+    """
+    return float(len(set(g1.node).intersection(set(g2.node)))) / float(len(set(g1.node).union(set(g2.node))))
+
+
+def subgraph_jin(pw1, pw2):
+    """
+    returns Jaccard index between two subgraphs from two nbunches 
+    
+    """
+    p1 = pw1.nodes
+    p2 = pw2.nodes
+    return float(len(set(p1).intersection(set(p2)))) / float(len(set(p1).union(set(p2))))
 
 
 def pathway_xtalk( g, ):
