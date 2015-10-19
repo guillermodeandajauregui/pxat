9c5467c31a4227ca8396a0070cc64da1dc8fd08e
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-10-19 17:50:55 -0500

    test stubs

--- test.py
+++ test.py
@@ -1,9 +1,99 @@
 import networkx as nx
 import pxat
+from pxat.KEGG import *
+from pxat.jin import *
+
 
 
 # run tests like
 # $ nosetests
+
+
+
+
+# test KEGG submodule
+def test_kgml_file_to_digraph():
+    pass
+
+def test_readKGML():
+    pass
+
+def test_kgml2graph():
+    pass
+
+
+
+
+# test JIN submodule
+def test_pathway_jin():
+    pass
+
+def test_graph_jin():
+    pass
+
+def test_subgraph_jin():
+    pass
+
+
+
+
+# test main module
+def test_merge_pathway_subgraphs():
+    pass
+
+def test_topological_annotate():
+    pass
+
+def test_topo_subgrapher():
+    pass
+
+def test_subgraph_from_pathways():
+    pass
+
+def test_nodes_by_subtype():
+    pass
+
+def test_disubgraph():
+    pass
+
+def test_crosstalk_nodes():
+    pass
+
+def test_pathway_xtalk():
+    pass
+
+def test_trajectories_in_graph():
+    pass
+
+def test_trajectories_from_nbunch():
+    pass
+
+def test_is_single_component():
+    pass
+
+def test_reduce_crosstalk_nodes():
+    pass
+
+def test_nodes_from_pathway():
+    pass
+
+def test_paths():
+    pass
+
+def test_targets_from_node():
+    pass
+
+def test_sources_to_node():
+    pass
+
+def test_write_adj_matrix():
+    pass
+
+
+
+
+
+
 
 
 def test_b():
@@ -12,6 +102,9 @@ def test_b():
    
 
 g = nx.gpickle.read_gpickle('red_prueba.pickle') 
+
+
+
 
 
 
