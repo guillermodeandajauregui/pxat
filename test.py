00bd49d1374715651e66db2dbc98a44cd1fb2799
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-10-19 17:38:55 -0500

    a possible test

--- test.py
+++ test.py
@@ -15,3 +15,24 @@ g = nx.gpickle.read_gpickle('red_prueba.pickle')
 
 
 
+
+
+#
+# seek trajectories in human kegg, prototest
+#
+
+# g = nx.gpickle.read_gpickle('human_kegg_ann.pickle')
+
+# signals   = set()
+# effectors = set()
+
+# for n in g.nodes():
+#     if 'signal' in g.node[n]:
+#         signals.add(n)
+
+#     if 'effector' in g.node[n]:
+#         effectors.add(n)
+
+# for s in signals:
+#     for e in effectors:
+#         print s, e, [p for p in nx.all_simple_paths(g, s, e)]
